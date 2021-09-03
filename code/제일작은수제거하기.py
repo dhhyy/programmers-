d4e7d3dbaa19069c8ee53f4fd87950877ac8a698
dongheon yu <ptw880@gmail.com>
2021-09-03 17:13:20 +0900

    작은수제거

--- code/제일작은수제거하기.py
+++ code/제일작은수제거하기.py
@@ -1,12 +1,26 @@
-# # min
+# def solution(arr):
+    
+#     return [i for i in arr if i > min(arr)]
+    
+# print(solution([4,3,2,1]))
+# print(solution([10]))
+
+arr = [4,3,2,1]
+
+result = []
+
+for i in arr:
+    if i > min(arr):
+        result.append(i)
+print(result)
+
 
 # def solution(arr):
     
-    
-data = [1,3,2,8,5,9]
+#     if(len(arr)==1):
+#         return [-1]
+#     else:
+#         arr.remove(min(arr))
 
-data.sort(reverse=True)
-
-del data[-1]
-
-print(data)
+#     answer = arr
+#     return answer
