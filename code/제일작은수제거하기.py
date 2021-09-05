fe698531edc4746768d051e9c2c8eaccc1c7c1a4
dongheon yu <ptw880@gmail.com>
2021-09-05 23:39:07 +0900

    정수제곱근

--- code/제일작은수제거하기.py
+++ code/제일작은수제거하기.py
@@ -1,26 +1,32 @@
-# def solution(arr):
+def solution(arr):
     
-#     return [i for i in arr if i > min(arr)]
+# 성공 케이스
+    if(len(arr)==1):
+        
+        return [-1]
     
-# print(solution([4,3,2,1]))
-# print(solution([10]))
+    else:
+        
+        arr.remove(min(arr))
 
-arr = [4,3,2,1]
+    answer = arr
+    return answer
 
-result = []
-
-for i in arr:
-    if i > min(arr):
-        result.append(i)
-print(result)
-
-
-# def solution(arr):
+# 실패 케이스 1 = 테스트케이스 1번 실패
+#     if len(arr) > 1:
+#         return [i for i in arr if i > min(arr)]
     
-#     if(len(arr)==1):
-#         return [-1]
 #     else:
-#         arr.remove(min(arr))
+#         return [-1]
 
-#     answer = arr
-#     return answer
+#               ***********************
+    
+# 실패 케이스 2 = 테스트케이스 1번 실패
+#     result = []
+    
+#     for i in arr:
+#         if i > min(arr):
+#             result.append(i)
+#         if len(arr) == 1:
+#             return [-1]
+#     return result
