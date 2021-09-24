b7484aebd2e8ffc863cf1f97270c4babea58ce1d
dongheon yu <ptw880@gmail.com>
2021-09-24 21:59:27 +0900

    두개

--- code/소수찾기.py
+++ code/소수찾기.py
@@ -1,30 +1,3 @@
-# def solution(n):
-    
-#     result = []
-#     # 2나 3으로 나눠지지 않으면서 1과 자기 자신만으로 나뉘어지는 수
-#     for i in range(1, n+1):
-#         if i % i == 0 and i % 1 == 0:
-#             result.append(i)
-#     return result
-
-# print(solution(10))
-# print(solution(5))
-
-
-# def solution(n):
-#     answer = 0
-#     result = []
-#     for i in range(1, n+1):
-#         if i % 2 == 0:
-#             result.append(i)
-#         if i % 3 == 0:
-#             result.append(i)
-        
-#     return result
-
-# print(solution(10))
-# print(solution(5))
-
 def solution(n):
     
     num_set = set(range(2, n+1))
@@ -43,4 +16,3 @@ def solution(n):
     return answer
 
 print(solution(10))
-# print(solution(5))
